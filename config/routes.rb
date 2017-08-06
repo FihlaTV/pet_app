4c14b24c72fb21683c14922bfe9ba03363bdc8b2
johnram528 <johnram528@gmail.com>
2017-08-05 22:56:22 -0400

    Adds authorization for the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
    namespace :api do 
     resources :business_units
     resources :cities
-    resources :users, only: [:show, :create]
+    resources :users, only: [:show, :create, :update, :destroy]
      resources :sessions, :only => [:create, :destroy]
   end
 end
